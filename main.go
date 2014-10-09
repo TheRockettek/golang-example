66e16c9ea876e84241f0ac276ae1d5bbdf93e577
Brad Heller <brad@cloudability.com>
2014-10-09 10:48:26 -0700

    Some comments about stuff. And things.

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ var (
 	Accumulator = int64(0)
 )
 
+// This function handles getting the current value of the accumulator. It will
+// return the current value (atomically) formatted as a string.
 func GetCount() string {
 	return fmt.Sprintf("%d", atomic.LoadInt64(&Accumulator))
 }
 
+// This function handles incrementing the counter atomically.
 func GetIncrement() string {
 	atomic.AddInt64(&Accumulator, 1)
 
