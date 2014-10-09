523fcb6904a0f730f5dfc2afae19bdfbae2e57d8
Brad Heller <brad@cloudability.com>
2014-10-09 10:46:33 -0700

    First pass at Counter example.

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"sync/atomic"
 	"github.com/go-martini/martini"
 )
 
+var (
+	Accumulator = int64(0)
+)
+
+func GetCount() string {
+	return fmt.Sprintf("%d", atomic.LoadInt64(&Accumulator))
+}
+
+func GetIncrement() string {
+	atomic.AddInt64(&Accumulator, 1)
+
+	// Empty body is OK. Status 200 indicates that it all worked.
+	return ""
+}
+
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
-	})
+	m.Get("/v1/increment", GetIncrement)
+	m.Get("/v1/count", GetCount)
 	m.Run()
 }
